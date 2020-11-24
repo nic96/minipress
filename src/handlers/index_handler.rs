04030b2e5f77f838158e0e9869703f07d7b2cd13
nic96 <nic96@users.noreply.github.com>
2020-11-23 20:00:33 -0700

    added handler for internal server error, and added user to data for error page

--- src/handlers/index_handler.rs
+++ src/handlers/index_handler.rs
@@ -7,14 +7,8 @@ use serde_json::json;
 #[get("/")]
 fn index(id: Identity, hb: web::Data<Handlebars<'_>>) -> HttpResponse {
     let logged_user = id.user();
-    let user_name = match &logged_user {
-        Some(u) => u.username.clone(),
-        None => "".to_string(),
-    };
 
     let data = json!({
-        "name": "Handlebars",
-        "user_name": user_name,
         "user": &logged_user,
     });
     let body = hb.render("index", &data).unwrap();
--- src/middleware/error_handlers.rs
+++ src/middleware/error_handlers.rs
@@ -1,15 +1,27 @@
+use crate::models::user::ToUser;
 use actix_http::{body::Body, Response};
+use actix_identity::Identity;
+use actix_web::dev::Payload;
 use actix_web::dev::ServiceResponse;
 use actix_web::http::StatusCode;
 use actix_web::middleware::errhandlers::{ErrorHandlerResponse, ErrorHandlers};
-use actix_web::web;
-use actix_web::Result;
+use actix_web::{web, FromRequest, Result};
 use handlebars::Handlebars;
 use serde_json::json;
 
 // Custom error handlers, to return HTML responses when an error occurs.
 pub fn error_handlers() -> ErrorHandlers<Body> {
-    ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found)
+    ErrorHandlers::new()
+        .handler(StatusCode::NOT_FOUND, not_found)
+        .handler(StatusCode::INTERNAL_SERVER_ERROR, internal_server_error)
+}
+
+// Error handler for a 404 Page not found error.
+fn internal_server_error<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+    let response = get_error_response(&res, "Internal server error.", "Internal error");
+    Ok(ErrorHandlerResponse::Response(
+        res.into_response(response.into_body()),
+    ))
 }
 
 // Error handler for a 404 Page not found error.
@@ -17,6 +29,7 @@ fn not_found<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
     let response = get_error_response(
         &res,
         "Whoopsy daisy! what were you trying to find? It seems that this page does not exists.",
+        "Page not found",
     );
     Ok(ErrorHandlerResponse::Response(
         res.into_response(response.into_body()),
@@ -24,7 +37,7 @@ fn not_found<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
 }
 
 // Generic error handler.
-fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> Response<Body> {
+fn get_error_response<B>(res: &ServiceResponse<B>, error: &str, title: &str) -> Response<Body> {
     let request = res.request();
 
     // Provide a fallback to a simple plain text response in case an error occurs during the
@@ -35,6 +48,12 @@ fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> Response<Body
             .body(e.to_string())
     };
 
+    let id = Identity::from_request(request, &mut Payload::None).into_inner();
+    let user = match id {
+        Ok(i) => i.user(),
+        Err(_) => None,
+    };
+
     let hb = request
         .app_data::<web::Data<Handlebars>>()
         .map(|t| t.get_ref());
@@ -42,7 +61,9 @@ fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> Response<Body
         Some(hb) => {
             let data = json!({
                 "error": error,
-                "status_code": res.status().as_str()
+                "title": title,
+                "status_code": res.status().as_str(),
+                "user": user, // user information is needed for the navbar/app_layout
             });
             let body = hb.render("error", &data);
 
